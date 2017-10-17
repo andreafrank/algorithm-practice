1b4cfcdaf2b621331b442a96e5afb405c48fd5e2
Andrea Frank <franklette70@gmail.com>
2017-10-17 10:28:52 -0500

    solve problem

--- find_pair.rb
+++ find_pair.rb
@@ -2,15 +2,16 @@
 # Given an unsorted array of integers, find a pair with a given sum in it
 
 def find_pair(sum, given_array)
+	pairs = given_array.combination(2).to_a
 
-	counter = Array.new
-
-	given_array.each do |number|
-		if number <= sum
-			counter << number until counter.reduce(:+) == sum
+	pairs.map do |pair|
+		addition = pair.reduce(:+)
+		if addition == sum
+			return pair
 		end
 	end
 
 end
 
 p find_pair(10,[ 2, 8, 9, 3, 11 ])
+p find_pair(100, [6, 90, 5, 4, 6, 8, 95])
