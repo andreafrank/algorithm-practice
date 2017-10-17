ea5cc31040dfea2ae61538ca1b538d61fc70acae
Andrea Frank <franklette70@gmail.com>
2017-10-17 00:34:06 -0500

    add method, not yet quite working

--- find_pair.rb
+++ find_pair.rb
@@ -1,6 +1,16 @@
 # Find Pair with given Sum in the Array 
 # Given an unsorted array of integers, find a pair with a given sum in it
 
-def find_pair(sum, array)
-	
+def find_pair(sum, given_array)
+
+	counter = Array.new
+
+	given_array.each do |number|
+		if number <= sum
+			counter << number until counter.reduce(:+) == sum
+		end
+	end
+
 end
+
+p find_pair(10,[ 2, 8, 9, 3, 11 ])
