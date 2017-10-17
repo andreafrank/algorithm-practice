cd77bafaaf565891b0406b156ae46688e33e012f
Andrea Frank <franklette70@gmail.com>
2017-10-17 10:32:15 -0500

    add comments

--- find_pair.rb
+++ find_pair.rb
@@ -2,10 +2,13 @@
 # Given an unsorted array of integers, find a pair with a given sum in it
 
 def find_pair(sum, given_array)
+	# separate given_array into all pair combinations
 	pairs = given_array.combination(2).to_a
 
+	# iterate through pairs to add each pair together
 	pairs.map do |pair|
 		addition = pair.reduce(:+)
+		#if the result of the addition adds up to the given sum, return this pair
 		if addition == sum
 			return pair
 		end
