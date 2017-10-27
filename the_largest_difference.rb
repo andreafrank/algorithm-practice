2ff054d24eb81a3d49cf63b4fd145966ad04b154
Andrea Frank <franklette70@gmail.com>
2017-10-27 17:40:41 -0500

    initial commit

---
+++ the_largest_difference.rb
@@ -0,0 +1,6 @@
+# Given an array of numbers, assume that they are all different. 
+# Calculate the largest difference (subtraction) between any two numbers of the array.
+
+# PSEUDOCODE
+# Find the min and the max
+# 
