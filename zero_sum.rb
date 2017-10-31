0db6d2e721bc4c8bb74ee7688a28930585af12b0
Andrea Frank <franklette70@gmail.com>
2017-10-30 23:06:06 -0500

    method works

--- zero_sum.rb
+++ zero_sum.rb
@@ -1,5 +1,21 @@
 # Given an array of integers, print all arrays having a 0 sum.
 
-def zero_sum(arr)
+# Needs refactoring; break if not given array or numbers, etc.
+# Will try another version using no built in Ruby methods.
 
+
+def zero_sum(given_arr)
+
+	all_combos = (1..given_arr.length).flat_map{ |number| given_arr.combination(number).to_a }
+	zero_sum_arr = []
+
+	all_combos.each do |combo|
+		if combo.reduce(:+) == 0
+			zero_sum_arr << combo
+		end
+	end
+	p zero_sum_arr
 end
+
+zero_sum([9, 4, 5, -4, -5])
+
