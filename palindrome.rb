87aea7b61bf7de3c7e455a27422056b0ab28df1b
Andrea Frank <franklette70@gmail.com>
2017-11-24 16:02:15 -0600

    flesh out method

--- palindrome.rb
+++ palindrome.rb
@@ -1,25 +1,28 @@
 # Given an array, output true or false if elements in array are a palindrome
 
-# PSEUDOCODE
-# 	consider if array is even or odd. if even, split and sort both sides of the palindrome. 
-# 	if odd, split and sort, remove last item from first array 
-# 	for even and odd, iterate through both split arrays and compare the position of each element.
-# 		if they are the same, print true, if no, false
 
 def palindrome(given_array)
 	arr_length = given_array.length
 
 	if arr_length.even?
-		# ensure that split is correct
-		given_array.split
+		split_arr = given_array.each_slice(arr_length/2) { |arr| p arr }
+		
 	elsif arr_length.odd?
-		p 'odd'
+		# chop off the middle element
+		p arr_length/2 
 	else 
 		p 'something went wrong. please try again.'
 	end
+
+	# arr_length = given_array.length
+
+	# if given_array.first == 
+
+
 
 
 end
 
 palindrome([1, 2, 3, 5, 3, 2, 1])
+palindrome([2, 3, 5, 5, 3, 2])
 
